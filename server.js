2a5d66dcde37231733efd6698547a2b2bc2d1d14
mednizar8058 <rockstarhard4@gmail.com>
2021-06-19 13:51:28 +0100

    add db connectivity

--- server.js
+++ server.js
@@ -2,6 +2,13 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb+srv://bino:Binoonib123123@cluster0.e2xpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{ useNewUrlParser: true },
+() => console.log("connected to the database !")
+);
 
 //import routes
 const authRoute = require('./routes/auth')
