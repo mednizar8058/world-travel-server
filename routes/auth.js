ae338fad82557d229517ab7d4682075701c24c52
mednizar8058 <rockstarhard4@gmail.com>
2021-06-21 20:30:52 +0100

    install joi for validation

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,27 @@ const router = require('express').Router();
 
 const User = require('../model/User')
 
+//user validation
+const Joi = require('@hapi/joi');
+const schema = Joi.object({
+    fname : Joi.string().min(6).required(),
+    lname : Joi.string().min(6).required(),
+    email : Joi.string().min(7).email().required(),
+    password : Joi.string().min(6).required()
+});
+
+
+
+
+
 //empty post route
 router.post('/registration', async (req,res) =>{
+    //validating the data before creating a user
+    
+    const {error} = schema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    
+
     const user = new User({
         fname : req.body.fname,
         lname : req.body.lname,
