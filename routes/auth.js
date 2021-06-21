966fffc2528c191294ba8717347f934d56b9922a
mednizar8058 <rockstarhard4@gmail.com>
2021-06-21 20:58:18 +0100

    hashing the password before saving the user

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 //this is a route
 const router = require('express').Router();
+const { required } = require('@hapi/joi');
 const User = require('../model/User');
 const {registerValidation} = require('../validation');
+const bcrypt = require('bcryptjs');
 
 //user validation
 
@@ -21,8 +23,11 @@ router.post('/registration', async (req,res) =>{
     if(emailExist) return res.status(400).send('email already exist');
 
     //now we need to hash the password --never store the password as plain text :( --
-     
-
+    //A salt is a random string. By hashing a plain text password plus a salt, 
+    // the hash algorithm’s output is no longer predictable. The same password will no longer yield the same hash.
+    // The salt gets automatically included with the hash, so you do not need to store it in a database.
+    const salt = await bcrypt.genSalt(10); 
+    const hashedPassword = await bcrypt.hash(req.body.password, hash);
 
 
     // create a user after validation
@@ -30,7 +35,7 @@ router.post('/registration', async (req,res) =>{
         fname : req.body.fname,
         lname : req.body.lname,
         email : req.body.email,
-        password : req.body.password
+        password : hashedPassword
     });
 
     try{
