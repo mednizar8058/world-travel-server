1a5523c5532fb22bea0ae1fe9160cb233b9982ca
mednizar8058 <rockstarhard4@gmail.com>
2021-06-22 01:21:34 +0100

    add login validation to auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,9 @@
 const router = require('express').Router();
 const { required } = require('@hapi/joi');
 const User = require('../model/User');
-const {registerValidation} = require('../validation');
+const {registerValidation, loginValidation} = require('../validation');
 const bcrypt = require('bcryptjs');
+const user = require('../model/User');
 
 //user validation
 
@@ -12,22 +13,22 @@ const bcrypt = require('bcryptjs');
 
 
 
-//empty post route
+//registration
 router.post('/registration', async (req,res) =>{
     //validating the data before creating a user
     const {error} = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
     // check if user exist
-    const emailExist = await User.findOne({email : req.body.email});
-    if(emailExist) return res.status(400).send('email already exist');
+    const userExist = await User.findOne({email : req.body.email});
+    if(userExist) return res.status(400).send('email already exist');
 
     //now we need to hash the password --never store the password as plain text :( --
     //A salt is a random string. By hashing a plain text password plus a salt, 
     // the hash algorithm’s output is no longer predictable. The same password will no longer yield the same hash.
     // The salt gets automatically included with the hash, so you do not need to store it in a database.
     const salt = await bcrypt.genSalt(10); 
-    const hashedPassword = await bcrypt.hash(req.body.password, hash);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
 
     // create a user after validation
@@ -44,6 +45,26 @@ router.post('/registration', async (req,res) =>{
     }catch(err){
         res.status(200).send(err);
     }
+});
+
+//login
+router.post('/login', async (req,res) =>{
+    //validating data
+    const {error} = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    // check if user exists
+    const userExist = await User.findOne({email : req.body.email});
+    if(!userExist) return res.status(400).send("check ur email or password");
+
+    //check if password is correct
+    const validPassword = await bcrypt.compare(req.body.password, userExist.password);
+    if(!validPassword) return res.status(400).send('invalid password');
+
+    res.send('login in succesful');
+
+    
+
 });
 
 module.exports = router;
